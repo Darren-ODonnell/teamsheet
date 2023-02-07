6fe3c9fb3cb628ded400fafdf8a802af71298998
Darren-ODonnell <Dar@ren01>
2023-02-07 22:22:16 +0000

    initial

---
+++ src/Teamsheet.js
@@ -0,0 +1,31 @@
+const clubs = {
+{ "name": "Castleknock", "irishName": "Cumann BÃ¡ire & Peile ChaisleÃ¡n Cnucha", "contactName": "Kevin Broderick", "contactEmail": "kevin.broderick4@icloud.ie", "contactPhone": "086 8064230", "pitches": "castleknock gaa club", "colours": "Blue & Yellow", "abbrev": "CKNK" },
+{ "name": "Ballinteer St Johns", "irishName": "Cumann Naomh Eoin Baile an tSaoir", "contactName": "Dara Taaffe", "contactEmail": "dtaaffe@businesspost.ie", "contactPhone": "086 8941042", "pitches": "Ballinteer St Johns GAA", "colours": "Tangerine & Black", "abbrev": "BSJN" },
+{ "name": "Ballyboden St Endas", "irishName": "CLG Baile BuadÃ¡in Naomh Ã‰anna", "contactName": "Terry O'Neill", "contactEmail": "terry.bb.oneill@gmail.com", "contactPhone": "087 7840982", "pitches": "Ballyboden St Enda's Gaa", "colours": "Blue and White", "abbrev": "BBSE" },
+{ "name": "Na Fianna", "irishName": "CLG Na Fianna", "contactName": "Austin Rock", "contactEmail": "rock.austin@gmail.com", "contactPhone": "087 6299356", "pitches": "CLG Na Fianna", "colours": "Yellow & Blue", "abbrev": "NAFN" },
+{ "name": "St. Marnocks", "irishName": "Naomh MearnÃ³g", "contactName": "Bernard Farrell", "contactEmail": "farrellhurls@hotmail.com", "contactPhone": "087 6738079", "pitches": "Naomh Mearnog GAA Club", "colours": "Black & Amber", "abbrev": "MEAR" },
+{ "name": "Lucan Sarsfields", "irishName": "SÃ¡irsÃ©alaigh LeamhcÃ¡in ", "contactName": "Dermot English", "contactEmail": "dcappa35@gmail.com", "contactPhone": "086 1649019", "pitches": "Lucan Sarsfields GAA Club", "colours": "Green & Black", "abbrev": "LUCN" },
+{ "name": "St Judes", "irishName": "Naomh Jude", "contactName": "Marion O'Donnell", "contactEmail": "marion.odonnell2@gmail.com", "contactPhone": "085 7345310", "pitches": "St. Jude's GAA Club", "colours": "Blue & Navyâ€‹", "abbrev": "JUDE" },
+{ "name": "St Vincents", "irishName": "Naomh Uinsionn", "contactName": "Patricia Murphy", "contactEmail": "hedgo@me.com", "contactPhone": "086 1925822", "pitches": "St. Vincent's GAA Club", "colours": "Blue & White", "abbrev": "VINN" },
+{ "name": "Kilmacud Crokes", "irishName": "Cill Mochuda na CrÃ³caigh ", "contactName": "Mark Cregg", "contactEmail": "mark.cregg@gmail.com", "contactPhone": "087 9828244", "pitches": "Kilmacud Crokes GAA Club", "colours": "Purple and Gold", "abbrev": "KCRK" },
+{ "name": "Faughs Celtic", "irishName": "CLG FÃ¡g an Bealach", "contactName": "Tom O'Mahony", "contactEmail": "tom.omahony.gpo.dublin@gaa.ie", "contactPhone": "086 8114643", "pitches": "Faughs GAA Club", "colours": "Green and Gold", "abbrev": "FAUG" },
+{ "name": "Cuala", "irishName": "Cuala", "contactName": "Donal Murphy", "contactEmail": "donal.murphy68@gmail.com", "contactPhone": "087 9182930", "pitches": "Cuala GAA Club", "colours": "Navy, Red & White", "abbrev": "CULA" },
+{ "name": "Skerries Harps", "irishName": "ClÃ¡irseacha na SceirÃ­", "contactName": "Terry GormleyÂ ", "contactEmail": "secretary.skerriesharps.dublin@gaa.ie", "contactPhone": "087 2313653", "pitches": "Skerries Harps GAA Club", "colours": "Blue & Navy", "abbrev": "SHRP" },
+{ "name": "St Oliver Plunkett/Eoghan Ruadh", "irishName": "Naomh OilibhÃ©ar PluincÃ©ad, Eoghan Ruadh", "contactName": "John Murphy", "contactEmail": "murphjohnd1965@gmail.com", "contactPhone": "087 6949445", "pitches": "St. Oliver Plunkett Eoghan Ruadh GAA Club", "colours": "Maroon & Gold", "abbrev": "OPER" },
+{ "name": "Good Counsel Liffey Gaels", "irishName": "Deagh Comhairle/Gaeil na Life", "contactName": "Ronnie Murphy", "contactEmail": "", "contactPhone": "086 8447492", "pitches": "Good Counsel GAA Club", "colours": "Black\White\Maroon", "abbrev": "GCLG" },
+{ "name": "Erins Isle", "irishName": "CLG OileÃ¡n na hÃ‰ireann", "contactName": "Louise O'Hara", "contactEmail": "", "contactPhone": "086 8730702", "pitches": "Erins Isle GAA Club", "colours": "Green & Black", "abbrev": "ERIN" },
+{ "name": "Crumlin", "irishName": "Croimghlinn", "contactName": "Ailish Murray", "contactEmail": "", "contactPhone": "086 3697089", "pitches": "Crumlin GAA (Pearse Park)", "colours": "Blue & White", "abbrev": "CRUM" }
+}
+
+const Teamsheet = () => {
+
+
+    return (
+
+
+
+    )
+
+}
+
+export default Teamsheet;
